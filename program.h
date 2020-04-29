a8aed30f621392fca5017d96ad076dea3f65b389
Timothy D Voisich <tdvoisic@timberlake.cse.buffalo.edu>
2020-04-29 00:53:47 -0400

    Added more function definitions and the neccessary structs

--- program.h
+++ program.h
@@ -14,6 +14,7 @@ struct Ingredient* ing;
 
 struct Book{
 struct Book* next;
+strcut Book* head;
 struct Recipe* food;
 
 }
@@ -23,8 +24,22 @@ char name[30];
 int calBGram;
 }
 
+struct Pantry{
+struct Ingredient* parts;
+int quantities;
+}
+
 struct Book * newBook();
 
 struct Recipe * newRecipe(char name[], int servings);
 
+struct Ingredient * newIngredient(char name[], int caloeriesPerGram);
 
+struct Pantry * newPantry();
+
+void addRecipe(struct Book* entry, struct Recipe* food);
+
+void addIngredient(struct Recipe* food, struct Ingredient* part, int quantity);
+
+void storeIngredient(struct Pantry* store, struct Ingredient* part, int quantity);
+ 
